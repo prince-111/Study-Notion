6067c788e16d7a964453e8e84a7fe206c0813e48
Prince Kumar <princesd991@gmail.com>
2024-05-19 16:57:02 +0530

    🛖 add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { useState } from "react";
+import Signup from "./pages/Signup";
+import PrivateRoute from "./components/PrivateRoute";
 
 export const App=()=> {
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="text-green-500 font-extrabold text-3xl">
-    Start Study Noiton
+    <div className="w-screen h-screen bg-richblack-900 flex-col">
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+
+      <Routes>
+        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/Signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
-
