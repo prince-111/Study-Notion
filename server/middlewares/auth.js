944be8a5731454effafe6227f8d24443de9f63b0
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2024-05-31 23:33:13 +0530

    🥎 add student middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -43,3 +43,22 @@ exports.auth = async (req, res, next) => {
     });
   }
 };
+
+// isStudent
+exports.isStudent = async (req, res, next) => {
+  try {
+    const userDetails = await User.findOne({ email: req.user.email });
+
+    if (userDetails.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a Protected Route for Students",
+      });
+    }
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `User Role Can't be Verified` });
+  }
+};
