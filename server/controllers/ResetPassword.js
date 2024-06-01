b03e3f8f3cad6cc0261416863adefba9664dadec
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2024-06-01 23:57:18 +0530

    🗼add reset password token

---
+++ server/controllers/ResetPassword.js
@@ -0,0 +1,49 @@
+const mailSender = require("../utils/mailSender");
+const User = require("../models/User");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+
+// resetPasswordToken
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: `This Email: ${email} is not Registered With Us Enter a Valid Email `,
+      });
+    }
+    const token = crypto.randomBytes(20).toString("hex");
+
+    const updatedDetails = await User.findOneAndUpdate(
+      { email: email },
+      {
+        token: token,
+        resetPasswordExpires: Date.now() + 3600000,
+      },
+      { new: true }
+    );
+    console.log("DETAILS", updatedDetails);
+
+    const url = `http://localhost:3000/update-password/${token}`;
+
+    await mailSender(
+      email,
+      "Password Reset",
+      `Your Link for email verification is ${url}. Please click this url to reset your password.`
+    );
+
+    res.json({
+      success: true,
+      message:
+        "Email Sent Successfully, Please Check Your Email to Continue Further",
+    });
+  } catch (error) {
+    return res.json({
+      error: error.message,
+      success: false,
+      message: `Some Error in Sending the Reset Message`,
+    });
+  }
+};
