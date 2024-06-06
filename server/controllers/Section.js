41d71f9d5408c492b6f1e30c6dd4382875758dd2
Prince Kumar <princesd991@gmail.com>
2024-06-06 23:24:26 +0530

    🍇 delete section controllers

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -86,3 +86,49 @@ exports.updateSection = async (req, res) => {
   }
 };
 
+// DELETE a section
+exports.deleteSection = async (req, res) => {
+  try {
+    const { sectionId, courseId } = req.body;
+    await Course.findByIdAndUpdate(courseId, {
+      $pull: {
+        courseContent: sectionId,
+      },
+    });
+    const section = await Section.findById(sectionId);
+    console.log(sectionId, courseId);
+    if (!section) {
+      return res.status(404).json({
+        success: false,
+        message: "Section not found",
+      });
+    }
+    // Delete the associated subsections
+    await SubSection.deleteMany({ _id: { $in: section.subSection } });
+
+    await Section.findByIdAndDelete(sectionId);
+
+    // find the updated course and return it
+    const course = await Course.findById(courseId)
+      .populate({
+        path: "courseContent",
+        populate: {
+          path: "subSection",
+        },
+      })
+      .exec();
+
+    res.status(200).json({
+      success: true,
+      message: "Section deleted",
+      data: course,
+    });
+  } catch (error) {
+    console.error("Error deleting section:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
